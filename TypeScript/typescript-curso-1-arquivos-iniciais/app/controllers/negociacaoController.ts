a0b085b2fe08d3c71991de3a665707ac1b110fea
Dan <danilobomfim.si@gmail.com>
2022-06-28 19:52:23 -0300

    Configurando o tsconfig.json etc

--- TypeScript/typescript-curso-1-arquivos-iniciais/app/controllers/negociacaoController.ts
+++ TypeScript/typescript-curso-1-arquivos-iniciais/app/controllers/negociacaoController.ts
@@ -11,17 +11,25 @@ export class NegociacaoController {
     this.inputValor = document.querySelector("#valor");
   }
 
-  adicionar() {
+  adicionar():void {
     const negociacao = this.criaNegociacao();
     console.log(negociacao)
+    this.limparFormulario();
   }
 
-  criaNegociacao() {
+  criaNegociacao(): Negociacao {
     const exp = /-/g;
     const date = new Date(this.inputData.value.replace(exp, ","));
     const quantidade = parseInt(this.inputQuantidade.value);
     const valor = parseFloat(this.inputValor.value);
 
     return new Negociacao(date, quantidade, valor);
+  }
+
+  limparFormulario(): void{
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputValor.value = '';
+    this.inputData.focus();
   }
 }
